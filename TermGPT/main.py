83b92d545f58d332579be76782dca8204d9f063d
Ruslan-Isaev <ruskakolbasa@xmail.ru>
2024-10-08 09:34:15 +0300

    Update main.py

--- TermGPT/main.py
+++ TermGPT/main.py
@@ -1,8 +1,26 @@
 import requests
 import io
+import sys
 
 def main_function():
-	question = input("GPT>>$ ")
+	arguments = sys.argv[1:]
+	if len(arguments) < 1:
+		print("Не указаны аргументы!\n\nПример использования:\ngpt {вопрос} - задает вопрос ChatGPT\ngpt --file {путь} - ищет текст, указанный в файле.\n\nСохранение контекста отсутствует!")
+		return
+	elif arguments[0] == "--file":
+		if len(arguments) > 1:
+			try:
+			             with open(arguments[1], 'r') as file:
+			             	question = file.read()  
+
+			except Exception as e:
+				print(f'Error: {e}'
+)
+				return
+		else:
+			print("Вы не указали путь!")
+	else:
+		question = ' '.join(arguments)
 	prompt = [{"role": "user", "content": question}]
 	response = requests.post('http://api.onlysq.ru/ai/v1', json=prompt)
 	response_json = response.json()
